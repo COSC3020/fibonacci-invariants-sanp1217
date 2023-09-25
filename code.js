bf433155f1b31060476021eb3023959b4deca959
pabs <pablosgrad12@gmail.com>
2023-09-24 18:42:06 -0600

    Adds code for fib

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function fib(n) {
-    return n;
+    if (n == 0) {
+        return [0];
+    } else if (n == 1) {
+        return [0, 1];
+    } else {
+        let fibNums = fib(n - 1);
+        fibNums.push(fibNums[fibNums.length - 1] + fibNums[fibNums.length - 2]);
+        return fibNums;
+    }
 }
